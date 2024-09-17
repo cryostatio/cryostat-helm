a15101e529aa832838848294fb8ff7319fdb8c30
Andrew Azores <aazores@redhat.com>
2024-09-17 14:27:51 -0400

    allow override of kubectl, ex. for 'oc'

--- ct.bash
+++ ct.bash
@@ -3,11 +3,12 @@
 set -xe
 
 TEST_NAMESPACE="${TEST_NAMESPACE:-helm-test}"
+CLUSTER_CLIENT="${CLUSTER_CLIENT:-$(which kubectl)}"
 
 function cleanup() {
-    kubectl delete ns "${TEST_NAMESPACE}"
+    "${CLUSTER_CLIENT}" delete ns "${TEST_NAMESPACE}"
 }
 trap cleanup EXIT
 
-kubectl create ns "${TEST_NAMESPACE}"
+"${CLUSTER_CLIENT}" create ns "${TEST_NAMESPACE}"
 ct install --remote "${REMOTE:-origin}" --target-branch "${TARGET_BRANCH:-main}" --namespace="${TEST_NAMESPACE}" --config ct.yaml --debug "$@"
