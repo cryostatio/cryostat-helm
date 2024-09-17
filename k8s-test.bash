7c0db5e0f89e9f27ee9c6d63af0c47a5a5d14dd1
Andrew Azores <aazores@redhat.com>
2024-09-17 14:31:19 -0400

    check for commands, overridable kubectl

--- k8s-test.bash
+++ k8s-test.bash
@@ -2,16 +2,32 @@
 
 set -xe
 
+CLUSTER_CLIENT="${CLUSTER_CLIENT:-$(which kubectl)}"
+
+if ! command -v "${CLUSTER_CLIENT}"; then
+    echo "${CLUSTER_CLIENT} command not found in \$PATH"
+    exit 1
+fi
+
+if ! command -v helm; then
+    echo "No 'helm' found in \$PATH"
+    exit 1
+fi
+
 TEST_NAMESPACE="${TEST_NAMESPACE:-k8s-test}"
 RELEASE_NAME="${RELEASE_NAME:-cryostat-k8s-test}"
 
 if [ "${CREATE_CLUSTER:-true}" = "true" ]; then
+    if ! command -v kind; then
+        echo "No 'kind' found in \$PATH"
+        exit 1
+    fi
     kind create cluster
 fi
 
-kubectl create ns "${TEST_NAMESPACE}"
+"${CLUSTER_CLIENT}" create ns "${TEST_NAMESPACE}"
 function cleanup() {
-    kubectl delete ns "${TEST_NAMESPACE}"
+    "${CLUSTER_CLIENT}" delete ns "${TEST_NAMESPACE}"
     if [ "${CREATE_CLUSTER:-true}" = "true" ]; then
         kind delete cluster
     fi
@@ -19,5 +35,5 @@ function cleanup() {
 trap cleanup EXIT
 
 helm install --namespace "${TEST_NAMESPACE}" "${RELEASE_NAME}" ./charts/cryostat
-kubectl wait --timeout=2m --for=condition=Ready=true --namespace "${TEST_NAMESPACE}" pod -l app.kubernetes.io/name=cryostat
+"${CLUSTER_CLIENT}" wait --timeout=2m --for=condition=Ready=true --namespace "${TEST_NAMESPACE}" pod -l app.kubernetes.io/name=cryostat
 helm test --namespace "${TEST_NAMESPACE}" "${RELEASE_NAME}"
